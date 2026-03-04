c3f723fd0bede3c6c23929d74c990f2102f17da8
2025linw <2021linw39@gmail.com>
2026-03-04 13:45:23 -0600

    improved date filter query for query_task, make db `query_tasks` function agnostic to URI format

--- src/com/model.rs
+++ src/com/model.rs
@@ -1,21 +1,8 @@
 mod tag;
 mod task;
 
+pub mod db;
 pub mod query;
 
 pub use tag::{Tag, TagQuery};
 pub use task::{Task, TaskQuery};
-
-use serde::Deserialize;
-use uuid::Uuid;
-
-#[derive(Debug, Deserialize)]
-pub struct PathTaskTag {
-    pub task_id: Uuid,
-    pub tag_id: Uuid,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct TaskTag {
-    pub tag_ids: Vec<Uuid>,
-}
