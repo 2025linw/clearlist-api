46be4d09eb290127061d960e8839cb3bc51ee05c
2025linw <2021linw39@gmail.com>
2026-03-31 08:37:24 -0500

    comment out tag test for later

--- src/db/tag.rs
+++ src/db/tag.rs
@@ -133,169 +133,170 @@ pub async fn delete_tag(conn: PgPool, tag_id: Uuid, user_id: Uuid) -> Result<Opt
     Ok(Some(()))
 }
 
-#[cfg(test)]
-mod query_tests {
-    use std::env;
-    use std::sync::OnceLock;
+// TODO: tag tests
+// #[cfg(test)]
+// mod query_tests {
+//     use std::env;
+//     use std::sync::OnceLock;
 
-    use tokio::test;
+//     use tokio::test;
 
-    use super::*;
+//     use super::*;
 
-    static DB_URL: OnceLock<String> = OnceLock::new();
+//     static DB_URL: OnceLock<String> = OnceLock::new();
 
-    fn get_connect_url() -> &'static str {
-        let conn = DB_URL.get_or_init(|| {
-            dotenvy::from_filename(".env.testing").ok();
+//     fn get_connect_url() -> &'static str {
+//         let conn = DB_URL.get_or_init(|| {
+//             dotenvy::from_filename(".env.testing").ok();
 
-            let url: String = env::var("DATABASE_URL").unwrap();
+//             let url: String = env::var("DATABASE_URL").unwrap();
 
-            url
-        });
+//             url
+//         });
 
-        // run migrations
+//         // run migrations
 
-        conn
-    }
+//         conn
+//     }
 
-    async fn setup() -> PgPool {
-        dotenvy::from_filename(".env.testing").ok();
+//     async fn setup() -> PgPool {
+//         dotenvy::from_filename(".env.testing").ok();
 
-        PgPool::connect(get_connect_url()).await.unwrap()
-    }
+//         PgPool::connect(get_connect_url()).await.unwrap()
+//     }
 
-    // WARN: tests should be agnostic to any given state of the database
-    // This means that the test asserts should not rely on specific values if not testing for them or guaranteed in another way
-    // For example:
-    //     - Don't check list length if not limiting by LIMIT
-    //     - Don't rely on order of the name ('Test Tag 1', 'Test Tag 2', etc...)
+//     // WARN: tests should be agnostic to any given state of the database
+//     // This means that the test asserts should not rely on specific values if not testing for them or guaranteed in another way
+//     // For example:
+//     //     - Don't check list length if not limiting by LIMIT
+//     //     - Don't rely on order of the name ('Test Tag 1', 'Test Tag 2', etc...)
 
-    // #[test]
-    // async fn query_base() {
-    //     let conn = setup().await;
+//     // #[test]
+//     // async fn query_base() {
+//     //     let conn = setup().await;
 
-    //     let opts = TagQueryOptions {
-    //         user_id: Uuid::nil(),
-    //         limit: None,
-    //         offset: None,
-    //         deleted: false,
-    //     };
+//     //     let opts = TagQueryOptions {
+//     //         user_id: Uuid::nil(),
+//     //         limit: None,
+//     //         offset: None,
+//     //         deleted: false,
+//     //     };
 
-    //     let tags = match query_tags(conn, opts).await {
-    //         Ok(tags) => tags,
-    //         Err(err) => {
-    //             panic!("{}", err);
-    //         }
-    //     };
+//     //     let tags = match query_tags(conn, opts).await {
+//     //         Ok(tags) => tags,
+//     //         Err(err) => {
+//     //             panic!("{}", err);
+//     //         }
+//     //     };
 
-    //     // WARN: test failure here (see above)
-    //     assert_eq!(tags.len(), 6);
-    //     for (n, tag) in (1..=6).zip(tags.iter()) {
-    //         assert_eq!(tag.label, format!("Test Tag {}", n))
-    //     }
-    // }
+//     //     // WARN: test failure here (see above)
+//     //     assert_eq!(tags.len(), 6);
+//     //     for (n, tag) in (1..=6).zip(tags.iter()) {
+//     //         assert_eq!(tag.label, format!("Test Tag {}", n))
+//     //     }
+//     // }
 
-    // #[test]
-    // async fn query_limit_3() {
-    //     let conn = setup().await;
+//     // #[test]
+//     // async fn query_limit_3() {
+//     //     let conn = setup().await;
 
-    //     let opts = TagQueryOptions {
-    //         user_id: Uuid::nil(),
-    //         limit: Some(3),
-    //         offset: None,
-    //         deleted: false,
-    //     };
+//     //     let opts = TagQueryOptions {
+//     //         user_id: Uuid::nil(),
+//     //         limit: Some(3),
+//     //         offset: None,
+//     //         deleted: false,
+//     //     };
 
-    //     let tags = match query_tags(conn, opts).await {
-    //         Ok(tags) => tags,
-    //         Err(err) => {
-    //             panic!("{}", err);
-    //         }
-    //     };
+//     //     let tags = match query_tags(conn, opts).await {
+//     //         Ok(tags) => tags,
+//     //         Err(err) => {
+//     //             panic!("{}", err);
+//     //         }
+//     //     };
 
-    //     // WARN: test failure here (see above)
-    //     assert_eq!(tags.len(), 3);
-    //     for (n, tag) in (1..=3).zip(tags.iter()) {
-    //         assert_eq!(tag.label, format!("Test Tag {}", n));
-    //     }
-    // }
+//     //     // WARN: test failure here (see above)
+//     //     assert_eq!(tags.len(), 3);
+//     //     for (n, tag) in (1..=3).zip(tags.iter()) {
+//     //         assert_eq!(tag.label, format!("Test Tag {}", n));
+//     //     }
+//     // }
 
-    // #[test]
-    // async fn query_limit_3_with_offset() {
-    //     let conn = setup().await;
+//     // #[test]
+//     // async fn query_limit_3_with_offset() {
+//     //     let conn = setup().await;
 
-    //     for i in 0..2 {
-    //         let i_min = i * 3;
-    //         let i_max = i_min + 3;
+//     //     for i in 0..2 {
+//     //         let i_min = i * 3;
+//     //         let i_max = i_min + 3;
 
-    //         let opts = TagQueryOptions {
-    //             user_id: Uuid::nil(),
-    //             limit: Some(3),
-    //             offset: Some(i * 3),
-    //             deleted: false,
-    //         };
+//     //         let opts = TagQueryOptions {
+//     //             user_id: Uuid::nil(),
+//     //             limit: Some(3),
+//     //             offset: Some(i * 3),
+//     //             deleted: false,
+//     //         };
 
-    //         let tags = match query_tags(conn.clone(), opts).await {
-    //             Ok(tags) => tags,
-    //             Err(err) => {
-    //                 panic!("{}", err);
-    //             }
-    //         };
+//     //         let tags = match query_tags(conn.clone(), opts).await {
+//     //             Ok(tags) => tags,
+//     //             Err(err) => {
+//     //                 panic!("{}", err);
+//     //             }
+//     //         };
 
-    //         assert_eq!(tags.len(), 3);
-    //         for (i, tag) in (i_min..=i_max).zip(tags.iter()) {
-    //             assert_eq!(tag.label, format!("Test Tag {}", i + 1));
-    //         }
-    //     }
-    // }
+//     //         assert_eq!(tags.len(), 3);
+//     //         for (i, tag) in (i_min..=i_max).zip(tags.iter()) {
+//     //             assert_eq!(tag.label, format!("Test Tag {}", i + 1));
+//     //         }
+//     //     }
+//     // }
 
-    // #[test]
-    // async fn query_deleted() {
-    //     let conn = setup().await;
+//     // #[test]
+//     // async fn query_deleted() {
+//     //     let conn = setup().await;
 
-    //     let opts = TagQueryOptions {
-    //         user_id: Uuid::nil(),
-    //         limit: None,
-    //         offset: None,
-    //         deleted: true,
-    //     };
+//     //     let opts = TagQueryOptions {
+//     //         user_id: Uuid::nil(),
+//     //         limit: None,
+//     //         offset: None,
+//     //         deleted: true,
+//     //     };
 
-    //     let tags = match query_tags(conn, opts).await {
-    //         Ok(tags) => tags,
-    //         Err(err) => {
-    //             panic!("{}", err);
-    //         }
-    //     };
+//     //     let tags = match query_tags(conn, opts).await {
+//     //         Ok(tags) => tags,
+//     //         Err(err) => {
+//     //             panic!("{}", err);
+//     //         }
+//     //     };
 
-    //     assert_eq!(tags.len(), 3);
-    //     for (n, tag) in (7..=9).zip(tags.iter()) {
-    //         assert_eq!(tag.label, format!("Test Tag {}", n));
-    //     }
-    // }
+//     //     assert_eq!(tags.len(), 3);
+//     //     for (n, tag) in (7..=9).zip(tags.iter()) {
+//     //         assert_eq!(tag.label, format!("Test Tag {}", n));
+//     //     }
+//     // }
 
-    // #[test]
-    // async fn query_ensure_order_consistency() {
-    //     let conn = setup().await;
+//     // #[test]
+//     // async fn query_ensure_order_consistency() {
+//     //     let conn = setup().await;
 
-    //     for _ in 0..10 {
-    //         let opts = TagQueryOptions {
-    //             user_id: Uuid::nil(),
-    //             limit: None,
-    //             offset: None,
-    //             deleted: false,
-    //         };
+//     //     for _ in 0..10 {
+//     //         let opts = TagQueryOptions {
+//     //             user_id: Uuid::nil(),
+//     //             limit: None,
+//     //             offset: None,
+//     //             deleted: false,
+//     //         };
 
-    //         let tags = match query_tags(conn.clone(), opts).await {
-    //             Ok(tags) => tags,
-    //             Err(err) => {
-    //                 panic!("{}", err);
-    //             }
-    //         };
+//     //         let tags = match query_tags(conn.clone(), opts).await {
+//     //             Ok(tags) => tags,
+//     //             Err(err) => {
+//     //                 panic!("{}", err);
+//     //             }
+//     //         };
 
-    //         assert_eq!(tags.len(), 6);
-    //         for (n, tag) in (1..=6).zip(tags.iter()) {
-    //             assert_eq!(tag.label, format!("Test Tag {}", n))
-    //         }
-    //     }
-    // }
-}
+//     //         assert_eq!(tags.len(), 6);
+//     //         for (n, tag) in (1..=6).zip(tags.iter()) {
+//     //             assert_eq!(tag.label, format!("Test Tag {}", n))
+//     //         }
+//     //     }
+//     // }
+// }
